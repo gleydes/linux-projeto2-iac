55cd0a9d96f947b10e00113b02509e4a78d424b2
gleydes <gley_dez-10@hotmail.com>
2022-07-30 13:30:15 -0300

    Linux Script Apache

---
+++ script-iac2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+apt-get update 
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+echo "Instalando os arquivos"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd /tmp/linux-site-dio-main
+cp -R * /var/www/html/
+
